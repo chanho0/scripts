b60a050bc20fd48332ae4a03cff3d71daa1f9e2e
chanho0 <53634547+chanho0@users.noreply.github.com>
2021-10-06 19:27:49 +0800

    sd

--- buchong/pull.sh
+++ buchong/pull.sh
@@ -1,1 +1,5 @@
+/*
+#拉取
+30 0,22 * * * pull.sh
+*/
 ql repo https://github.com/Aaron-lv/sync.git "jd_dreamFactory.js|jdzz|jxnc|bookshop|cash|sgmh|cfd.js|health|moneyTree" "jddj|collect|exchange|open|activity|backUp" "^jd[^_]|USER|utils" "jd_scripts"
