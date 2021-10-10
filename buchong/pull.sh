d40f3dad615c1475545e2b44a0477000e0e5f2ba
chanho0 <53634547+chanho0@users.noreply.github.com>
2021-10-10 21:22:37 +0800

    s

--- buchong/pull.sh
+++ buchong/pull.sh
@@ -1,3 +1,3 @@
 #*/10 * * * * pull.sh, tag=拉取
-ql repo https://github.com/Aaron-lv/sync.git "jdzz|jxnc|bookshop|cash|sgmh|cfd.js|health" "jddj|collect|exchange|open|activity|backUp" "^jd[^_]|USER|utils" "jd_scripts"
+ql repo https://github.com/Aaron-lv/sync.git "jdzz|jxnc|bookshop|cash" "jddj|collect|exchange|open|activity|backUp" "^jd[^_]|USER|utils" "jd_scripts"
 ql repo https://github.com/JDHelloWorld/jd_scripts.git "abcd" "" "TS_USER_AGENTS"
