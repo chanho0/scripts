17622d5e4f43b5a96fae32b59d2fbdc261a4c814
chanho0 <53634547+chanho0@users.noreply.github.com>
2021-11-01 10:06:57 +0800

    s

--- buchong/pull.sh
+++ buchong/pull.sh
@@ -2,4 +2,4 @@
 ql repo https://github.com/Aaron-lv/sync.git "jdzz|jxnc|bookshop|joy_run" "jddj|collect|exchange|open|activity|backUp" "^jd[^_]|USER|utils" "jd_scripts"
 ql repo https://github.com/JDHelloWorld/jd_scripts.git "stock|UpdateCron" "" "TS_USER_AGENTS"
 ql repo https://github.com/Tsukasa007/my_script.git "wskey|1600" "" "" "master"
-#ql repo https://github.com/smiek2221/scripts.git "abcd" "open" "ZooFaker_Necklace.js|JDJRValidator_Pure.js|sign_graphics_validate.js"
+#ql repo https://github.com/smiek2121/scripts.git "carnivalcity" "open" "ZooFaker_Necklace.js|JDJRValidator_Pure.js|sign_graphics_validate.js"
