b1cbb66a5a8d27849889b3e86f06507eea320846
chanho0 <53634547+chanho0@users.noreply.github.com>
2021-11-05 01:34:07 +0800

    s

--- buchong/pull.sh
+++ buchong/pull.sh
@@ -1,5 +1,5 @@
 #*/10 * * * * pull.sh, tag=拉取
 ql repo https://github.com/Aaron-lv/sync.git "jdzz|jxnc|bookshop|joy_run" "jddj|collect|exchange|open|activity|backUp" "^jd[^_]|USER|utils" "jd_scripts"
-ql repo https://github.com/JDHelloWorld/jd_scripts.git "stock|UpdateCron" "" "TS_USER_AGENTS"
+ql repo https://github.com/JDHelloWorld/jd_scripts.git "stock|newCron" "" "TS_USER_AGENTS"
 ql repo https://github.com/Tsukasa007/my_script.git "wskey|1600" "" "" "master"
 #ql repo https://github.com/smiek2121/scripts.git "carnivalcity" "open" "ZooFaker_Necklace.js|JDJRValidator_Pure.js|sign_graphics_validate.js"
